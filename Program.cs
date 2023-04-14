ca5a2c8e60145cd3d76b71a34d2b67bda0497f10
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-14 19:39:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            double n2;
+            double Media;
+
+            Console.Write("Insira o valor do Primeiro numero: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Insira o valor do Segundo numero: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            Media = Math.Sqrt(n1 * n2);
+            Console.WriteLine("A Média Geométrica dos dois numeros é {0}.", Media);
+        }
+    }
+}
